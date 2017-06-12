2e31a3d56fdcd83efcbc964c41799a94835ac173
Chris Werner Rau <CWRau@CWRCoding.com>
2017-06-12 16:41:38 +0200

    Added script to get certificate for a list of domains Added domainlist

---
+++ sites-default/updatecertificate.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+#	https://stackoverflow.com/a/246128
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+domainsParameter=""
+
+while read domain; do
+	domainsParameter="${domainsParameter} -d ${domain}"
+done <${DIR}/certlist
+
+echo ${domainsParameter}
+
+read -p "Get certificates for those domains? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	service nginx stop
+	certbot-2 certonly --standalone --rsa-key-size 4096 $domainsParameter
+	service nginx start
+fi
