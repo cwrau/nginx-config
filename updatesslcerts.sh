a2a4692240a4e2d6a75f2f318901a91b3b39f5d3
Chris Werner Rau <CWRau@CWRCoding.com>
2018-01-12 00:35:03 +0100

    Add new service - rocketchat under chat.cwrcoding.com domain

--- updatesslcerts.sh
+++ updatesslcerts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 STAGING=${1:-1}
+COMMAND=${2:-renew}
 sslPath="../tmp"
 acmeVolume="acme_staging"
 letsencryptVolume="letsencrypt_staging"
@@ -11,7 +12,7 @@ then
     letsencryptVolume="letsencrypt"
 fi
 
-dockerOutput=$(docker run -v $(realpath "$sslPath"):/var/ssl -v $acmeVolume:/etc/acme -v acme_challenge:/var/acme_challenge_webroot -v $letsencryptVolume:/etc/letsencrypt -v $(realpath ./domains.txt):/etc/domains.txt -e RSA_KEY_LENGTH=4096 -e ECDSA_KEY_LENGTH=ec-384 -e MUST_STAPLE=1 -e CHALLENGE_MODE=webroot -e DOMAINS=/etc/domains.txt -e STAGING=$STAGING asamoshkin/letsencrypt-certgen renew 2>&1)
+dockerOutput=$(docker run -v $(realpath "$sslPath"):/var/ssl -v $acmeVolume:/etc/acme -v acme_challenge:/var/acme_challenge_webroot -v $letsencryptVolume:/etc/letsencrypt -v $(realpath ./domains.txt):/etc/domains.txt -e RSA_KEY_LENGTH=4096 -e ECDSA_KEY_LENGTH=ec-384 -e MUST_STAPLE=1 -e CHALLENGE_MODE=webroot -e DOMAINS=/etc/domains.txt -e STAGING=$STAGING asamoshkin/letsencrypt-certgen $COMMAND 2>&1)
 
 echo -e "$dockerOutput"
 
@@ -19,6 +20,6 @@ if echo -e "$dockerOutput" | grep -q -v "Cert not yet due for renewal" && false
 then
     if [ "$STAGING" = 0 ]
     then
-        docker-compose exec -T nginx nginx -s reload
+        docker-compose down && docker-compose up -d
     fi
 fi
